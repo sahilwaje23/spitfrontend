2ac6964e8daefa4455eb4417c2c4291ce74bd595
sahilwaje23 <your-email@example.com>
2025-02-01 15:49:09 +0530

    spit

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
-import {CartProvider} from "./context/cartContext.jsx";
+import {CartProvider} from "./context/CartContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
